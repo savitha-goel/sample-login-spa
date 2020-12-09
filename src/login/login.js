1010cbe35f6245f5a0498fa54666fece0f5b26f1
savitha-goel <savitha_goel@yahoo.com>
2020-12-08 17:39:00 -0800

    convert component to functional react for login

--- src/login/login.js
+++ src/login/login.js
@@ -1,81 +1,76 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
 
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthApi from '../services/authApi';
 import './_login.css';
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.onSignIn = this.onSignIn.bind(this);
+function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
 
-    this.state = {
-      userName: '',
-      password: ''
-    };
-  }
+  const handleUserNameChange = e => {
+    setUserName(e.target.value);
+  };
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSignIn() {
+  const onSignIn = () => {
     let userData = {
-      'user_name': this.state.userName,
-      'password': this.state.password
+      'user_name': userName,
+      'password': password
     };
+
     AuthApi.login(userData).then((response) => {
       if (response.data.p_out_mssg === 'Success') {
-        this.props.history.push('/dashboard');
+        history.push('/dashboard');
       } else {
-        this.props.history.push('/unauthorized');
+        history.push('/unauthorized');
       }
     }).catch(() => {
-      this.props.history.push('/unauthorized');
+      history.push('/unauthorized');
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="col d-flex justify-content-center">
-        <div className="card card-signin my-5 shadow-lg">
-          <div className="card-body">
-            <h5 className="card-title text-center">Sign In</h5>
-            <form>
-              <div className="form-group">
-                <label htmlFor="userName">User Name:</label>
-                <input
-                  type="text"
-                  id="userName"
-                  name="userName"
-                  className="form-control"
-                  placeholder="Enter User Name"
-                  onChange={this.handleChange.bind(this)}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="form-control password"
-                  placeholder="Enter your password"
-                  onChange={this.handleChange.bind(this)}
-                />
-              </div>
-              <div className="float-right">
-                <button type="button" onClick={this.onSignIn} className="btn btn-secondary btn-sm custom-btn-class">
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
+  return (
+    <div className="col d-flex justify-content-center">
+      <div className="card card-signin my-5 shadow-lg">
+        <div className="card-body">
+          <h5 className="card-title text-center">Sign In</h5>
+          <form>
+            <div className="form-group">
+              <label htmlFor="userName">User Name:</label>
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                className="form-control"
+                placeholder="Enter User Name"
+                onChange={handleUserNameChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="form-control password"
+                placeholder="Enter your password"
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div className="float-right">
+              <button type="button" onClick={() => onSignIn()} className="btn btn-secondary btn-sm custom-btn-class">
+              Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withRouter(Login);
+export default Login;
